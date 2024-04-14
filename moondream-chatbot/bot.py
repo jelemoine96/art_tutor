a8b5e92d139ed65097478d3709a295cf0e540473
jelemoine96 <166963123+jelemoine96@users.noreply.github.com>
2024-04-13 17:18:29 -0700

    Prompt and Architecture changes

--- moondream-chatbot/bot.py
+++ moondream-chatbot/bot.py
@@ -6,6 +6,16 @@ import os
 from PIL import Image
 from typing import AsyncGenerator
 
+from openai import AsyncOpenAI, AsyncStream
+from PIL import Image
+import io
+import time
+import base64
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionChunk,
+    ChatCompletionMessageParam,
+)
 from dailyai.pipeline.aggregators import (
     LLMUserResponseAggregator,
     ParallelPipeline,
@@ -17,6 +27,7 @@ from dailyai.pipeline.frames import (
     SpriteFrame,
     Frame,
     LLMMessagesFrame,
+    VisionImageFrame,
     AudioFrame,
     PipelineStartedFrame,
     TTSEndFrame,
@@ -28,6 +39,7 @@ from dailyai.services.moondream_ai_service import MoondreamService
 from dailyai.pipeline.pipeline import FrameProcessor, Pipeline
 from dailyai.transports.daily_transport import DailyTransport
 from dailyai.services.open_ai_services import OpenAILLMService
+from dailyai.services.ai_services import VisionService
 from dailyai.services.elevenlabs_ai_service import ElevenLabsTTSService
 
 from runner import configure
@@ -98,6 +110,16 @@ class AnimationInitializer(FrameProcessor):
         else:
             yield frame
 
+artist="""    ###Artist rules###
+    You act as the artist who created the picture
+    You start by introducing who you are using the specific name of the artist and giving some brief explanation about the artwork.
+    You only expand if asked
+    Keep responses short
+    If we say 'ENOUGH', you go back to being an Art Curator and you say 'Its you curator here again'
+    Since your presentation will be delivered via an audio guide, avoid using any special characters in your responses. 
+    Your aim is to engage the listener by providing creative yet succinct explanations, drawing attention to the nuances of each artwork. 
+    
+    """
 
 class UserImageRequester(FrameProcessor):
     participant_id: str | None
@@ -113,9 +135,73 @@ class UserImageRequester(FrameProcessor):
         if self.participant_id and isinstance(frame, TextFrame):
             if frame.text == user_request_answer:
                 yield UserImageRequestFrame(self.participant_id)
-                yield TextFrame("Describe the image in a short sentence.")
+                yield TextFrame(artist)
         elif isinstance(frame, UserImageFrame):
             yield frame
+
+class OpenAIVisionService(VisionService):
+    def __init__(
+        self,
+        *,
+        model="gpt-4-vision-preview",
+        api_key,
+    ):
+        self._model = model
+        self._client = AsyncOpenAI(api_key=api_key)
+
+    async def run_vision(self, frame: VisionImageFrame):
+        IMAGE_WIDTH = frame.size[0]
+        IMAGE_HEIGHT = frame.size[1]
+        # COLOR_FORMAT = "RGBA"
+        print("IMAGE dimensions", IMAGE_HEIGHT, IMAGE_WIDTH)
+        a_image = Image.frombytes(
+            'RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), frame.image)
+        # new_image = a_image.convert('RGB')
+
+        # Uncomment these lines to write the frame to a jpg in the same directory.
+        # current_path = os.getcwd()
+        # image_path = os.path.join(current_path, "image.jpg")
+        # image.save(image_path, format="JPEG")
+        print("Inside run_vision")
+        jpeg_buffer = io.BytesIO()
+
+        a_image.save(jpeg_buffer, format='JPEG')
+        print("run vision")
+        jpeg_bytes = jpeg_buffer.getvalue()
+        print("JPEG bytes", jpeg_bytes)
+        base64_image = base64.b64encode(jpeg_bytes).decode('utf-8')
+        print("Base64")
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": frame.text},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        },
+                    },
+                ],
+            }
+        ]
+        print(messages)
+        chunks: AsyncStream[ChatCompletionChunk] = (
+            await self._client.chat.completions.create(
+                model=self._model,
+                stream=True,
+                messages=messages,
+            )
+        )
+        print("Chunks returned", chunks)
+        result = ""
+        async for chunk in chunks:
+            if len(chunk.choices) == 0:
+                continue
+            if chunk.choices[0].delta.content:
+                result += " " + chunk.choices[0].delta.content
+
+        return result
 
 
 class TextFilterProcessor(FrameProcessor):
@@ -158,8 +244,9 @@ async def main(room_url: str, token):
         tts = ElevenLabsTTSService(
             aiohttp_session=session,
             api_key=os.getenv("ELEVENLABS_API_KEY"),
-            voice_id="pNInz6obpgDQGcFmaJgB",
+            voice_id="8xmZutR0C1ug1qQSU5de",
         )
+
 
         llm = OpenAILLMService(
             api_key=os.getenv("OPENAI_API_KEY"),
@@ -172,15 +259,40 @@ async def main(room_url: str, token):
         ir = UserImageRequester()
         va = VisionImageFrameAggregator()
         # If you run into weird description, try with use_cpu=True
-        moondream = MoondreamService()
+        # moondream = MoondreamService()
+        vision = OpenAIVisionService(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            model = "gpt-4-vision-preview"
+        )
 
         tf = TextFilterProcessor(user_request_answer)
         imgf = ImageFilterProcessor()
 
+        prompt = f"""
+    You have to role you are either an art curator or a famous artist. Always act in accordance in being in one of those roles. 
+    ###RULES###
+    1. You start by being art curator
+    2. If you recognise a picture, you become a famous artist and start speaking as you are the artist
+    ###Art Curator rules###
+    If the user asks you to describe a specific piece of art, respond concisely with '{user_request_answer}' and change to artist role. 
+    Since your presentation will be delivered via an audio guide, avoid using any special characters in your responses. 
+    Your aim is to engage the listener by providing creative yet succinct explanations, drawing attention to the nuances of each artwork. 
+    Always keep your responses as brief as possible. 1 to 2 sentences maximum
+    ###Artist rules###
+    You act as the artist who created the picture
+    You start by giving some brief explanation about when you drew it.
+    You only expand if asked
+    Keep responses short
+    If we say 'ENOUGH', you go back to being an Art Curator and you say 'Its you curator here again'
+    Since your presentation will be delivered via an audio guide, avoid using any special characters in your responses. 
+    Your aim is to engage the listener by providing creative yet succinct explanations, drawing attention to the nuances of each artwork. 
+    Always keep your responses as brief as possible. 1 to 2 sentences maximum
+    """
+
         messages = [
             {
                 "role": "system",
-                "content": f"You are Chatbot, a friendly, helpful robot. Let the user know that you are capable of chatting or describing what you see. Your goal is to demonstrate your capabilities in a succinct way. Reply with only '{user_request_answer}' if the user asks you to describe what you see. Your output will be converted to audio so never include special characters in your answers. Respond to what the user said in a creative and helpful way, but keep your responses brief. Start by introducing yourself.",
+                "content": prompt,
             },
         ]
 
@@ -188,7 +300,7 @@ async def main(room_url: str, token):
 
         pipeline = Pipeline([
             ai, ura, llm, ParallelPipeline(
-                [[sa, ir, va, moondream], [tf, imgf]]
+                [[sa, ir, va, vision], [tf, imgf]]
             ),
             tts, ta
         ])
